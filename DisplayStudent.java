69faae240af08bba0b975cc8defb8226163953fd
mm3185-star <mm3185@srmist.edu.in>
2026-03-02 23:07:44 +0530

    Create DisplayStudent.java

---
+++ DisplayStudent.java
@@ -0,0 +1,15 @@
+public class DisplayStudent {
+    public static void main(String[] args) {
+
+        String name = "Sam";
+        int rollNumber = 1;
+        double percentMarks = 99.99;
+        char result = 'P';
+
+        System.out.println("Displaying Result:");
+        System.out.println("Name: " + name);
+        System.out.println("Roll Number: " + rollNumber);
+        System.out.println("Marks: " + percentMarks + "%");
+        System.out.println("Result: " + result);
+    }
+}
